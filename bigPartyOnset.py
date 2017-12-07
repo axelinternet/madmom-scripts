5795514cd8d83b1aae06492b72fd92f9394cc105
axelinternet <axel@hultman.info>
2017-12-07 16:21:34 +0100

    Fixed so that all methods are covered properly

--- bigPartyOnset.py
+++ bigPartyOnset.py
@@ -7,12 +7,15 @@
 
 [X] 1. Functionality for more onset detectors.
 
-[ ] 2. Save to big-ass file. (Check the not used
-bin scripts that have an args.save if-else thingy)
+[ ] 2. Variable parameters
+       Add them as parameters from this file instead.
 
-[X] 3. Talk to the man
+[ ] 3. Save to big-ass file.
+       Fix the loop marked TODO.
+       Add output of what parameters were used to each row. 
 
-[ ] (4. Variable parameters)
+[X] 4. Talk to the man
+
 
 ******************************************
 
@@ -37,7 +40,7 @@ from small_result import small_result
 from madmom.evaluation.onsets import OnsetEvaluation 
 
 SOUND_FOLDER = 'BassDrumsSax_Single/'
-
+ONSET_METHODS = ['SuperFluxProcess', 'CNNProcess', 'ComplexFluxProcess', 'OnsetDetectorProcess (RNN)']
 def print_single_result(arr,name, filename):
     j = 0
     print('\nResults for {}:\n'.format(filename))
@@ -106,20 +109,22 @@ def save_results_to_file(results_list):
         if not os.path.exists('output/results_{}.csv'.format(nr)):
             with open('output/results_{}.csv'.format(nr), 'w+') as csvfile:
                 foundPath = True
-                stats_writer = csv.writer(csvfile)
+                stats_writer = csv.writer(csvfile, delimiter=',', quotechar='|')
                 # Write header row
-                stats_writer.writerow(['filename','num_annotations','num_tp','num_fp','num_fn','precision','recall','fmeasure','mean_error','std_error'])
+                stats_writer.writerow(['filename','onset_method', 'num_annotations','num_tp','num_fp','num_fn','precision','recall','fmeasure','mean_error','std_error'])
                 for result in results_list:
-                    stats_writer.writerow([ str(list(result.keys())[0]).strip('.wav'),
-                        list(result.values())[0][0]['num_annotations'],
-                        list(result.values())[0][0]['num_tp'],
-                        list(result.values())[0][0]['num_fp'],
-                        list(result.values())[0][0]['num_fn'],
-                        list(result.values())[0][0]['precision'],
-                        list(result.values())[0][0]['recall'],
-                        list(result.values())[0][0]['fmeasure'],
-                        list(result.values())[0][0]['mean_error'],
-                        list(result.values())[0][0]['std_error'] ])
+                    for i, onset_specific_result in enumerate(list(result.values())[0]):
+                        stats_writer.writerow([ str(list(result.keys())[0]).strip('.wav'),
+                            ONSET_METHODS[i],
+                            float(onset_specific_result['num_annotations']),
+                            float(onset_specific_result['num_tp']),
+                            float(onset_specific_result['num_fp']),
+                            float(onset_specific_result['num_fn']),
+                            float(onset_specific_result['precision']),
+                            float(onset_specific_result['recall']),
+                            float(onset_specific_result['fmeasure']),
+                            float(onset_specific_result['mean_error']),
+                            float(onset_specific_result['std_error'])])
         else:
             nr +=1;
 
@@ -161,7 +166,7 @@ def analyze_clip(sound_folder, filename, verbose=False):
         formated_result['std_error'] = evaluation_result.std_error * 1000
         clip_stats.append(formated_result)
     if verbose: 
-        print_single_result(clip_stats, ['SuperFluxProcess', 'CNNProcess', 'ComplexFluxProcess', 'OnsetDetectorProcess (RNN)'])
+        print_single_result(clip_stats, ONSET_METHODS)
     
     return clip_stats
 
